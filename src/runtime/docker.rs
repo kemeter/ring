931f80c7d5fa08ea405a98f298dde1e9e28da09f
Mbechezi Mlanawo <mlanawo.mbechezi@alpacode.io>
2022-08-04 17:36:07 +0200

    remove debug

--- src/runtime/docker.rs
+++ src/runtime/docker.rs
@@ -34,7 +34,6 @@ pub(crate) async fn apply(mut config: Deployment) -> Deployment {
         }
     } else {
         let number_instances: usize = config.instances.len();
-        info!("Instance {:?}", instance);
 
         if number_instances < config.replicas.try_into().unwrap() {
             info!("create container {}", config.image.clone());
