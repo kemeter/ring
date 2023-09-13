ab953f336ce7c74661b8514d50cd88bb05379bc5
Mbechezi Mlanawo <mlanawo.mbechezi@alpacode.io>
2023-09-13 22:57:13 +0200

    Fix init

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -3,6 +3,7 @@ use clap::SubCommand;
 use clap::ArgMatches;
 use std::fs;
 use rusqlite::Connection;
+use crate::config::config::get_config_dir;
 
 pub(crate) fn command_config<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name("init")
@@ -11,6 +12,6 @@ pub(crate) fn command_config<'a, 'b>() -> App<'a, 'b> {
 
 pub(crate) fn init(_args: &ArgMatches, connection: Connection) {
 
-    let migration_init = fs::read_to_string("./src/migrations/00.init.sql").unwrap();
-    connection.execute(&migration_init, []).unwrap();
+    fs::create_dir_all(get_config_dir()).expect("Unable to create config directory");
+    fs::write(format!("{}/auth.json", get_config_dir()), "{}").expect("Unable to create auth.json");
 }
